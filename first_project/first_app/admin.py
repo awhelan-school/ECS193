4aaff6662e3d9ba2ac67afca76e3bcb929990f0a
awhelan-school <awhelan@ucdavis.edu>
2018-05-05 17:27:59 -0700

    Add files via upload

--- first_project/first_app/admin.py
+++ first_project/first_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from first_app.models import AccessRecord, Topic, Query, Article
+from first_app.models import AccessRecord, Topic, Query, Article, ModelInfo
 
 # Username: admin
 # Email: awhelan@ucdavis.edu
@@ -9,6 +9,7 @@ admin.site.register(AccessRecord)
 admin.site.register(Topic)
 admin.site.register(Query)
 admin.site.register(Article)
+admin.site.register(ModelInfo)
 
 
 # Register your models here.
